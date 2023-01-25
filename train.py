f83e9d8845ab91b7631f927774cc67060b01f60a
GabrielBeFr <gabriel.benzenou@student-cs.fr>
2023-01-25 16:41:19 +0100

    Architecture of train.py

---
+++ train.py
@@ -0,0 +1,28 @@
+import torch
+
+def train():
+    # define device
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    # define loss_function
+    
+    # define optimizer and learning rate
+
+    # connect to wandb.ai
+
+    # define metrics
+
+    # define summary.txt content
+
+    # define transform functions (normalization)
+
+    # get dataloaders
+
+    # get model
+
+    # training loop
+
+
+
+if __name__ == '__main__':
+    train()
