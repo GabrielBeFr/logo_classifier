0245970dbdbaeb334bb88abbc4d56b86765d6c03
GabrielBeFr <gabriel.benzenou@student-cs.fr>
2023-02-03 17:53:20 +0100

    feat: added get_offset to utils

--- utils.py
+++ utils.py
@@ -1,5 +1,13 @@
 import yaml
 from yaml.loader import SafeLoader
+import h5py
+import numpy as np
+
+def get_offset(f: h5py.File) -> int:
+    external_id_dset = f["external_id"]
+    array = external_id_dset[:]
+    non_zero_indexes = np.flatnonzero(array)
+    return int(non_zero_indexes[-1]) + 1
 
 def get_config(config_file: str):
     with  open('config.yaml') as f:
