a882efca0407e3387c2c9c930163311e00c06e0c
GabrielBeFr <gabriel.benzenou@student-cs.fr>
2023-02-15 11:23:43 +0100

    feat: confusion+prohibit_classes+metrics+save_model

--- train.py
+++ train.py
@@ -6,21 +6,20 @@ import pathlib
 from dataset import get_dataloader
 import tqdm
 import sklearn.metrics
+from sklearn.preprocessing import normalize
+import matplotlib.pyplot as plt
 import numpy as np
-from datetime import datetime
-import json
-import os
 
 class LinearClassifier(nn.Module):
     def __init__(self, input_dim, output_dim):
         super().__init__()
         self.linear = nn.Linear(input_dim, output_dim)
         
-    def predict(self, x):
+    def forward(self, x):
         x = self.linear(x)
         return x
 
-def train(run, log_path, size_epoch = 10000, epochs: int=1):
+def train(files_dir: str, size_epoch = 10000, epochs: int=1, prohibited_classes: list=[], valid_no_class: bool=False, debugging: bool=False):
     # define device
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -30,7 +29,7 @@ def train(run, log_path, size_epoch = 10000, epochs: int=1):
     train_path = pathlib.Path("datasets/train_dataset.hdf5")
     val_path = pathlib.Path("datasets/val_dataset.hdf5")
     test_path = pathlib.Path("datasets/test_dataset.hdf5")
-    train_loader, val_loader, test_loader = get_dataloader(train_path, val_path, test_path, debugging = False)
+    train_loader, val_loader, test_loader = get_dataloader(train_path, val_path, test_path, prohibited_classes = prohibited_classes, debugging = debugging)
 
     # get model
     input_dim = 512 # number of features in the input data
@@ -51,7 +50,7 @@ def train(run, log_path, size_epoch = 10000, epochs: int=1):
     y_train = []
     y_test = []
     y_pred = []
-    classes_str, classes_ids = get_labels("datasets/class_infos.jsonl")
+    classes_str, classes_ids = get_labels("datasets/class_infos.jsonl", prohibited_classes=prohibited_classes)
 
     # training loop
     for epoch in range(epochs):
@@ -62,7 +61,7 @@ def train(run, log_path, size_epoch = 10000, epochs: int=1):
         for embeddings, labels, ids in tqdm.tqdm(train_loader):
             count += 1
             optimizer.zero_grad()
-            outputs = model.predict(embeddings.to(device))
+            outputs = model.forward(embeddings.to(device))
             loss = criterion(outputs, labels.to(device))
             loss.backward()
             optimizer.step()
@@ -72,17 +71,18 @@ def train(run, log_path, size_epoch = 10000, epochs: int=1):
             if count >= size_epoch:
                 break
 
-
         # Evaluate the model on validation set
         model.eval()
         val_loss = 0.0
         correct = 0
         total = 0
         missed_logos = {}
+        y_test = []
+        y_pred = []
         with torch.no_grad():
             print("Entering testing loop")
             for embeddings, labels, ids in tqdm.tqdm(val_loader):
-                outputs = model.predict(embeddings.to(device))
+                outputs = model.forward(embeddings.to(device))
                 loss = criterion(outputs, labels.to(device))
                 val_loss += loss.item()
                 _, predicted = torch.max(outputs.data, 1)
@@ -91,46 +91,105 @@ def train(run, log_path, size_epoch = 10000, epochs: int=1):
                 correct += (predicted == ground_truth.to(device)).sum().item()
                 for indice in torch.where((predicted == ground_truth.to(device))==False)[0].tolist():
                     missed_logos[str(ids[indice].item())]=[ground_truth[indice].item(),predicted[indice].item()]
+
+                # to compute the metrics, we take out all no_class logos (not the ones predicted as no_class but the true no_class)
+                predicted = predicted[torch.where(ground_truth!=0)]
+                ground_truth = ground_truth[torch.where(ground_truth!=0)]
                 y_pred += predicted.tolist()
                 y_test += ground_truth.tolist()
 
-        # Compute metrics
-        report = sklearn.metrics.classification_report(y_test, y_pred, labels=classes_ids, target_names=classes_str)
-        f1_micro = sklearn.metrics.f1_score(y_true=y_test, y_pred=y_pred, labels = classes_ids, average="micro")
-        f1_macro = sklearn.metrics.f1_score(y_true=y_test, y_pred=y_pred, labels = classes_ids, average="macro")
-        f1_classes = sklearn.metrics.f1_score(y_true=y_test, y_pred=y_pred, labels = classes_ids, average=None)
-        total_accuracy = 100 * correct / total
-        training_loss = train_loss / len(train_loader)
-        validation_loss = val_loss / len(val_loader)
-        confusion_matrix = sklearn.metrics.confusion_matrix(y_test, y_pred)
-        metrics_dict = {
-            "epoch": epoch + 1,
-            "f1_micro": f1_micro,
-            "f1_macro": f1_macro,
-            "total_accuracy": total_accuracy,
-            "training_loss": training_loss,
-            "validation_loss": validation_loss,
-            }
+        # Compute metrics and save model
+        current_macro_f1 = compute_metrics(y_test, y_pred, classes_ids, classes_str, len(train_loader), len(val_loader), correct, total, train_loss, val_loss, epoch)
         
-        for i in range(len(f1_classes)):
-            metrics_dict["f1 by class/f1_" + str(classes_str[i])] = f1_classes[i]
-        wandb.log(metrics_dict)
+        save_best_model = SaveBestModel()
+        save_best_model(current_macro_f1, epoch, model, device, files_dir)
 
-        print(f"report: {report}")
-        print(f"confusion_matrix: {confusion_matrix}")
+def compute_metrics(y_test, y_pred, classes_ids, classes_str, len_train_loader, len_val_loader, correct, total, val_loss, train_loss, epoch):
+    report = sklearn.metrics.classification_report(y_test, y_pred, labels=classes_ids, target_names=classes_str, zero_division=0)
+    f1_micro = sklearn.metrics.f1_score(y_true=y_test, y_pred=y_pred, labels = classes_ids, average="micro")
+    f1_macro = sklearn.metrics.f1_score(y_true=y_test, y_pred=y_pred, labels = classes_ids, average="macro", zero_division=0)
+    f1_classes = sklearn.metrics.f1_score(y_true=y_test, y_pred=y_pred, labels = classes_ids, average=None, zero_division=0)
+    total_accuracy = 100 * correct / total
+    training_loss = train_loss / len_train_loader
+    validation_loss = val_loss / len_val_loader
+    confusion_matrix = sklearn.metrics.confusion_matrix(y_test, y_pred)
+    confusion_matrix = normalize(confusion_matrix, axis=1)
+    metrics_dict = {
+        "epoch": epoch + 1,
+        "f1_micro": f1_micro,
+        "f1_macro": f1_macro,
+        "total_accuracy": total_accuracy,
+        "training_loss": training_loss,
+        "validation_loss": validation_loss,
+        }
+    
+    for i in range(len(f1_classes)):
+        metrics_dict["f1 by class/f1_" + str(classes_str[i])] = f1_classes[i]
+    wandb.log(metrics_dict)
+
+    print(f"report: {report}")
+    print(f"confusion_matrix: {confusion_matrix}")
+    fig = plt.figure(figsize=(24, 20))
+
+    plt.imshow(confusion_matrix, cmap=plt.get_cmap("Blues"))
+    ax = plt.gca()
+    ax.set_xticks(np.arange(-.5, len(classes_str)-1, 1))
+    ax.set_yticks(np.arange(-.5, len(classes_str)-1, 1))
+    ax.set_xticklabels(
+        classes_str,
+        size='smaller',
+        rotation='vertical',
+        )
+    ax.set_yticklabels(
+        classes_str,
+        size='smaller',
+        rotation='horizontal',
+        )
+    ax.grid(color='black', linestyle='-', linewidth=1)
+    plt.colorbar()
+    plt.ylabel("Predicted")
+    plt.xlabel("True")
+    wandb.log({"plot":fig})
+
+    return f1_macro
+
+
+class SaveBestModel:
+    """
+    Class to save the best model while training. If the current epoch's 
+    macro f1 is better than the previous best, then save the
+    model state.
+    """
+    def __init__(
+        self, best_macro_f1=-float('inf')
+    ):
+        self.best_macro_f1 = best_macro_f1
+        
+    def __call__(
+        self, current_macro_f1, 
+        epoch, model, device,
+        dir_path, batch_size = 32
+    ):
+        if current_macro_f1 > self.best_macro_f1:
+            self.best_macro_f1 = current_macro_f1
+            print(f"\nBest macro f1: {self.best_macro_f1}")
+            print(f"\nSaving best model for epoch: {epoch+1}\n")
+            torch.onnx.export(
+                model,
+                torch.zeros([batch_size, 512]).to(device),
+                dir_path+'/logos_classifier.onnx',
+                export_params=True,
+                verbose=True,
+                input_names = ['embeddings'],
+                output_names = ['scores_per_classes'],
+                dynamic_axes = {'embeddings':[0]}
+                )
+
 
 if __name__ == '__main__':
     run = wandb.init(project="logos-classifier")
-
-    dt_string = datetime.now().strftime("%d/%m/%Y %H:%M:%S").replace("/","_")
-    day, hour = dt_string.split(" ")
+    files_dir = run.dir
     
-    dict_path = pathlib.Path("manual_logs/"+day)
-    if not os.path.exists(dict_path):
-        os.mkdir(dict_path)
-    log_path = "manual_logs/"+day+"/"+hour+".json"
-
     config = get_config("config.yaml")
-    run = 5
-    train(run, log_path, size_epoch=10000, epochs=10)
+    train(files_dir=files_dir, size_epoch=1680, epochs=20, prohibited_classes=[], valid_no_class=False, debugging=False)
     run.finish()
